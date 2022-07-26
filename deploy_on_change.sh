32a414aa81870edb2e5d4ceae788b2344d98d103
TuLX <lexuantu1002@gmail.com>
2022-07-26 14:53:03 +0700

    update deploy_on_change.sh behavior

--- deploy_on_change.sh
+++ deploy_on_change.sh
@@ -4,17 +4,20 @@
 # - package inotify-tools
 # - place at project root dir
 
+# usage: `./deploy_on_change.sh DIR_TO_WATCH CANISTER_NAME`
+# example: `./deploy_on_change.sh ./src/day_1 day_1`
+
 ## UTILS
 
 deploy_canisters() {
-	dfx deploy
-	echo "<=== Canisters deployed on $1 $2 in $3 ===>"
+	dfx deploy "$4"
+	echo -e "<=== Canisters deployed on $1 $2 in $3 ===>\n"
 }
 
 watch_change() {
 	inotifywait --monitor --exclude declarations --recursive --quiet --event modify,move,delete $1 |
 		while read DIRECTORY EVENT FILE; do
-			deploy_canisters "$EVENT" "$FILE" "$DIRECTORY"
+			deploy_canisters "$EVENT" "$FILE" "$DIRECTORY" "$2"
 		done
 }
 
@@ -27,5 +30,5 @@ if ! command -v inotifywait &>/dev/null; then
 else
 	#command exists
 	echo "Watching $1"
-	watch_change "$1"
+	watch_change "$1" "$2"
 fi
